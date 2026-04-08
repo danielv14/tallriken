e85a1fbdd1b0a301291504e7e19251c47ba04f0a
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2026-04-08 16:34:40 +0200

    Add missing PhotoIcon import

--- src/routes/recipes/$recipeId.tsx
+++ src/routes/recipes/$recipeId.tsx
@@ -14,6 +14,7 @@ import {
   ClipboardDocumentIcon,
   CheckIcon,
   SparklesIcon,
+  PhotoIcon,
 } from '@heroicons/react/24/outline'
 
 export const Route = createFileRoute('/recipes/$recipeId')({
